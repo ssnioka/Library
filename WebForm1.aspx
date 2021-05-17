2f088914dc8bbc6fb73236d67de2633d52c8f325
ssnioka <53944632+ssnioka@users.noreply.github.com>
2021-05-17 14:18:39 +0300

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="_4_laboratorinis.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Laboras</title>
+    <style>
+        body{
+            background-image: url("https://www.photoblog.com/learn/wp-content/uploads/2019/02/kev-costello-467080-unsplash.jpg");
+            font-weight: bold;
+            color: white;
+            background-color: #fefbd8;
+            
+        }
+    </style>
+</head>
+<body style="height: 580px">
+    <form id="form1" runat="server">
+        <div style="height: 640px">
+            U4_16. Biblioteka.<br />
+            <br />
+            2 ir daugiau metų senumo leidinių skaičius:
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+            Visi moksliniai leidiniai:<asp:Table ID="Table1" runat="server" BorderStyle="Solid" GridLines="Both">
+            </asp:Table>
+            <br />
+            Visų nenaujų leidinių sąrašas:<asp:Table ID="Table2" runat="server" BorderStyle="Solid" GridLines="Both">
+            </asp:Table>
+            <br />
+            <br />
+            Leidiniai, kurių tiražas viršija 10 000 vnt:<br />
+            <asp:Table ID="Table3" runat="server" BorderStyle="Solid" GridLines="Both">
+            </asp:Table>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using _4_laboratorinis.Classes;
+
+namespace _4_laboratorinis
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        public List<Library> Libraries = new List<Library>();
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            InOutUtils.DeleteOldFiles();
+
+            try
+            {
+                Session["Libraries"] = InOutUtils.ReadFiles("1");
+                Libraries = (List<Library>)Session["Libraries"];
+            }
+            catch (FormatException ex)
+            {
+                Label2.Text = "Error: " + ex.Message + '\n' + ex.StackTrace + '\n' +  ex.Source;
+                Label2.Style["Color"] = "Red";
+            }
+            catch (FileNotFoundException ex)
+            {
+                Label2.Text = "Error: " + ex.Message + ex.StackTrace + ex.Source;
+                Label2.Style["Color"] = "Red";
+            }
+
+            /////////////////////////////////////////////////////////////////////////
+            List<Publication> Publications = TaskUtils.GetAllPublications(Libraries);
+            List<Publication> Books = TaskUtils.FilterBooks(Publications);
+            List<Publication> Journals = TaskUtils.FilterJournals(Publications);
+            List<Publication> Newspapers = TaskUtils.FilterNewspapers(Publications);
+            List<Publication> filteredBooks = TaskUtils.Sort(Books);
+            List<Publication> filteredJournals = TaskUtils.Sort(Journals);
+            List<Publication> filteredNewspapers = TaskUtils.Sort(Newspapers);
+            /////////////////////////////////////////////////////////////////////////
+            List<Publication> allPublications = TaskUtils.MergedLists(filteredBooks, filteredJournals, filteredNewspapers);
+
+            InOutUtils.PrintToTXT(Libraries);
+          
+            Label1.Text = TaskUtils.OldPublicationsCount(allPublications).ToString();
+
+            TaskUtils.FindScientificRecords(Table1, allPublications);
+            TaskUtils.FindOldPublications(Table2, allPublications);
+            TaskUtils.FindMostCopies(Table3, allPublications);
+
+            InOutUtils.PrintOldPublicationsCSV(allPublications);
+            InOutUtils.AlotOfCopiesCSV(allPublications);
+
+        }
+
+    
+    }
+}
